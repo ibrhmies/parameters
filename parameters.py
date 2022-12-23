0afecffcca014f90e3a3c3bfd4ce1cea6a3df86e
ibrhmies <02220201044@ogr.inonu.edu.tr>
2022-12-23 23:05:04 +0300

    parameters

---
+++ parameters.py
@@ -0,0 +1,11 @@
+def topla(a,b):
+    return a+b
+sonuc = topla(45,89)
+print(sonuc)
+#yukarıda yazdığımız fonksiyona bir parametre girdik a ve b değerlerini alıp toplayan program
+dogumYili =int(input("Doğum yıllınızı giriniz: "))
+def yasHesapla(dogumYili):
+    return 2022-dogumYili
+sonuc = yasHesapla(dogumYili)
+print(sonuc)
+#yukarıya girdiğiniz doğum yılına göre yaşınızı hesaplayan bir program yazdım
